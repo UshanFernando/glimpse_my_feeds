0e287478a0e26e50b8b7c0c5fbe4ffc005b0e6b6
ushanfernando <ushansankalpafernando@gmail.com>
2021-06-12 15:26:08 +0530

    first commit

---
+++ lib/screens/ViewFeed.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ViewFeed extends StatelessWidget {
+  const ViewFeed() : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
